f640bf1746dc911a6ad3b8b88af397adf9a7d7b9
nfederico <nfederico@live.com>
2019-10-27 23:28:11 -0300

    middleware de auth en implementacion

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import {Router} from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import AuthMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/users',UserController.store);
 routes.post('/sessions',SessionController.store);
+
+//routes.use('/users',AuthMiddleware);  //todo lo que viene abajo usa ese Middleware osea seria global
+//routes.put('/users',UserController.update);
+routes.put('/users',AuthMiddleware,UserController.update);
 
 export default routes;
