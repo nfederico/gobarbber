8e56f3b7e3e4306fe9cffa781037db1b8948ccd5
nfederico <nfederico@live.com>
2019-11-06 00:39:29 -0300

    Create appointment

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
 import AuthMiddleware from './app/middleware/auth';
 
 const routes = new Router();
@@ -18,5 +20,6 @@ routes.put('/users',UserController.update);
 // routes.put('/users',AuthMiddleware,UserController.update);
 routes.post('/files',upload.single('file'),FileController.store);
 routes.get('/providers',ProviderController.index);
+routes.post('/appointments',AppointmentController.store);
 
 export default routes;
