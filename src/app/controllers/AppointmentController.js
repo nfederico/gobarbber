20d87bcb4195d11775b24bcd25090c83b2c0e1c2
nfederico <nfederico@live.com>
2019-11-10 15:54:26 -0300

    Appointment Validatetion created and running

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import Appointment from '../models/Appointment';
+import {startOfHour,parseISO,isBefore} from 'date-fns';
 import * as Yup from 'yup';
 import User from '../models/User';
 
@@ -20,13 +21,37 @@ class AppointmentController {
        * 
        */
         const isProvider = await User.findOne({where: {id:provider_id,provider:true}});
-     if(!isProvider){
+        if(!isProvider){
          return res.status(401).json({error:'you can only create appointments with providers' });
-     }
+        }
+
+      /**
+       * Check if the appointment date is in the future 
+       * 
+       */
+        const hourStart= startOfHour(parseISO(date));
+        if (isBefore(hourStart,new Date())) {
+          return res.status(400).json({error:'Past dates are note allowed!'})  ;
+        }
+        /**
+       * Check date availability 
+       * 
+       */
+        const  checkAppointmentAvailability = await Appointment.findOne({where:{
+            provider_id,
+            cancelled_at: null,
+            date: hourStart,
+        }});
+
+        if (checkAppointmentAvailability) {
+            return res.status(400).json({error:'Appointment date is not available'})  ;
+        }
+
         const appointment = await Appointment.create({
             user_id:req.userId,
             provider_id,
-            date,
+            date: hourStart,
+           
         });
        return res.json(appointment);
     }
